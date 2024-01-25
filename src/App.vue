5502890ee2004efc9b18cb2f080111001c3dbea9
lpitto86 <luca.p481@gmail.com>
2024-01-25 12:14:16 +0100

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,124 +1,18 @@
 <script setup>
-import { ref } from 'vue';
-
-// HEADER
-
-const navItems = ref([
-  {
-    text: 'Characters',
-    url: '#',
-    active: false,
-  },
-  {
-    text: 'Comics',
-    url: '#',
-    active: true,
-  },
-  {
-    text: 'Movies',
-    url: '#',
-    active: false,
-  },
-  {
-    text: 'TV',
-    url: '#',
-    active: false,
-  },
-  {
-    text: 'Games',
-    url: '#',
-    active: false,
-  },
-  {
-    text: 'Collectibles',
-    url: '#',
-    active: false,
-  },
-  {
-    text: 'Videos',
-    url: '#',
-    active: false,
-  },
-  {
-    text: 'Fans',
-    url: '#',
-    active: false,
-  },
-  {
-    text: 'News',
-    url: '#',
-    active: false,
-  },
-  {
-    text: 'Shop',
-    url: '#',
-    active: false,
-  },
-]);
+import AppHeader from './components/AppHeader.vue'
 </script>
 
 <template>
-<!-- HEADER -->
   <header>
-        <div class="container">
-            <nav class="d-flex justify-between align-center">
-                <a class="logo-container" href="#">
-                    <img src="../../assets/img/dc-logo.png" alt="logo" title="logo">
-                </a>
+    <AppHeader/>
+  </header>
 
-                <ul class="d-flex">
-                    <li class="text-uppercase d-flex" :class="(navItem.active) ? 'active' : ''"
-                        v-for="navItem in navItems">
-                        <a :href="(navItem.url)" :alt="navItem.text">{{ navItem.text }}</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+  <main>
+  </main>
+
+  <footer>
+  </footer>
 </template>
 
 <style scoped>
-header {
-    background-color: #yourPrimaryColor; /* Sostituisci con il tuo colore primario */
-
-    .container {
-        padding-top: 0;
-        padding-bottom: 0;
-    }
-
-    nav {
-        height: 100px;
-
-        .logo-container {
-            width: 60px;
-        }
-
-        ul {
-            height: 100%;
-
-            li {
-                margin-right: 2.3rem;
-                line-height: 100px;
-
-                &.active {
-                    border-bottom: 3px solid #yourComplementaryColor; /* Sostituisci con il tuo colore complementare */
-
-                    a {
-                        color: #yourComplementaryColor; /* Sostituisci con il tuo colore complementare */
-                    }
-                }
-
-                a {
-                    font-size: 0.7rem;
-                    font-weight: bold;
-                    color: #yourDarkerIconBgColor; /* Sostituisci con il tuo colore per l'icona più scura */
-
-                    &:hover {
-                        color: #yourSecondaryColor; /* Sostituisci con il tuo colore secondario */
-                    }
-                }
-            }
-        }
-    }
-}
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <div class="container mt-4 mb-4">
+        <div class="row">        
+            <div class="col-4">
+                <img src="../assets/img/dc-logo.png" alt="logo">
+            </div>
+            <div class="col-8 d-flex justify-content-end">
+                <nav class="d-flex align-items-center justify-content-center">
+                    <ul class="list-unstyled d-flex gap-4">
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    a {
+    text-decoration: none;
+    color: black;
+    }
+</style>
